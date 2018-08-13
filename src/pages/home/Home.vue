c0762c6cac8ebe4b6dddbd1059e383095ef28f71
vf2e <2982074447@qq.com>
2018-08-13 14:20:17 +0800

    项目初始化

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="home">home</div>
+    <router-link to="/list">list</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+  .home{
+    color: red;
+  }
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="list">list</div>
+    <router-link to="/">home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style>
+  .list{
+    color: green;
+  }
+</style>
