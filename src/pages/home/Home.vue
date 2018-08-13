b4456e07bfabf77f8d9902a594d9b82be0afe4d6
vf2e <2982074447@qq.com>
2018-08-13 15:13:47 +0800

    add iconfont

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <div class="home">home</div>
-    <router-link to="/list">list</router-link>
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <div class="header-left">back</div>
+    <div class="header-input">search</div>
+    <div class="header-right">city</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    height: .86rem
+    line-height: .86rem
+    background: #2a95dd
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      border-radius: .1rem
+      margin-top: .12rem
+      margin-left: .1rem
+      background: #fff
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+</style>
