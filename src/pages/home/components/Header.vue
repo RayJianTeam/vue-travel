68a509911a217e0d0dcc2fc1cb36c951cb240145
vf2e <2982074447@qq.com>
2018-08-13 15:39:41 +0800

    add HomeHeader compontent

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="header">
-    <div class="header-left">back</div>
-    <div class="header-input">search</div>
-    <div class="header-right">city</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe601;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont search-icon">&#xe627;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      <span class="iconfont menu-icon">&#xe786;</span>
+    </div>
   </div>
 </template>
 
@@ -13,15 +20,19 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
   .header
     display: flex
     height: .86rem
     line-height: .86rem
-    background: #2a95dd
+    background: $bgColor
     color: #fff
     .header-left
-      width: .64rem
+      width: .88rem
       float: left
+      .back-icon
+        font-size: .36rem
+        text-align: center
     .header-input
       flex: 1
       height: .64rem
@@ -29,10 +40,16 @@ export default {
       border-radius: .1rem
       margin-top: .12rem
       margin-left: .1rem
+      padding-left: .2rem
       background: #fff
       color: #ccc
+      .search-icon
+        font-size: .32rem
+        margin-right: .06rem
     .header-right
-      width: 1.24rem
+      width: .88rem
       float: right
       text-align: center
+      .menu-icon
+        font-size: .4rem
 </style>
